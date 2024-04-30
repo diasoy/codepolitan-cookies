44afcd5dc3e41699d473dd145f715c81415dd695
diasoy <22082010134@student.upnjatim.ac.id>
2024-04-30 07:58:07 +0700

    add session

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const secret = "secret-key";
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("secret-key"));
+app.use(cookieParser(secret));
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.get("/signincookie", (req, res) => {
   res.cookie("paket", "ransel", { signed: true });
@@ -14,6 +24,23 @@ app.get("/signincookie", (req, res) => {
 app.get("/verifycookie", (req, res) => {
   const cookies = req.signedCookies;
   res.send(cookies);
+});
+
+app.get("/count", (req, res) => {
+  let count = req.cookies.count || 0;
+  count++;
+  res.cookie("count", count);
+  res.send(`Count: ${count}`);
+});
+
+app.get("/register", (req, res) => {
+  const { username = "Anonim" } = req.query;
+  req.session.username = username;
+  res.redirect("/dashboard");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("Welcome back to dasbhboard, " + req.session.username);
 });
 
 app.use("/admin", require("./routes/admin"));
